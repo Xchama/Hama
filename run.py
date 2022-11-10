bc3927fbeadea5f07b234be17f7eb1ae5fef7b7b
Xchama <117845547+Xchama@users.noreply.github.com>
2022-11-10 17:59:37 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,20 +1,5 @@
-import os, platform, time
-print('Checking Updates...')
-time.sleep(2)
+import os
 os.system('git pull')
-os.system('xdg-open https://t.me/i4m_Tokyo')
-time.sleep(2)
-os.system('xdg-open https://t.me/i4m_Tokyo')
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('git pull')
-import requests
-bit = platform.architecture()[0]
-elif bit == '32bit':
-    import hama
-    hama.o()
 
 import hama
 hama.o()
