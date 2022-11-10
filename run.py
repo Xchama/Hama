72b6448c25e6ead0f4da2459b23486221de755fc
Xchama <117845547+Xchama@users.noreply.github.com>
2022-11-10 16:18:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,20 @@
-import os
-if __name__ == "__main__":
-   try:
-       os.system("git pull")
-       __import__("hama").menu()
-   except Exception as e:
-       exit(str(e))
+import os, platform, time
+print('Checking Updates...')
+time.sleep(2)
+os.system('git pull')
+os.system('xdg-open https://www.youtube.com/c/MrQureshiTech')
+time.sleep(2)
+os.system('xdg-open https://www.youtube.com/channel/UCyUzwD8WYuICl5xf4CGZA5w')
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import hama
+    hama.o()
+elif bit == '32bit':
+    import hama
+    hama.o()
